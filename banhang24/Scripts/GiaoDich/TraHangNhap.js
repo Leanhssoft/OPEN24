283dd2853d2a02e17af50639ac4fad27c37989a6
NhuongDT <inftech.ts91@gmail.com>
2022-10-10 12:02:11 +0700

    - get list TraHangNhap: pass {ArrTrangThai}

--- banhang24/Scripts/GiaoDich/TraHangNhap.js
+++ banhang24/Scripts/GiaoDich/TraHangNhap.js
@@ -587,10 +587,10 @@
                     Post_NhatKySuDung_UpdateGiaVon(diary);
                 }
                 else {
-                    ShowMessage_Success('Hủy hóa đơn thất bại');
+                    ShowMessage_Danger('Hủy hóa đơn thất bại');
                 }
             }).fail(function () {
-                ShowMessage_Success('Hủy hóa đơn thất bại');
+                ShowMessage_Danger('Hủy hóa đơn thất bại');
             });
         });
     };
@@ -649,7 +649,7 @@
             arrStatus.push('0');
         }
         if (self.TT_DaHuy()) {
-            arrStatus.push('2');
+            arrStatus.push('4');
         }
         var statusInvoice = 1;
         if (self.TT_TamLuu()) {
@@ -818,6 +818,7 @@
             MaHoaDon: txtMaHDon,
             maHDGoc: '',
             TrangThai: statusInvoice,
+            ArrTrangThai: arrStatus,
             DayStart: dayStart,
             DayEnd: dayEnd,
             arrChiNhanh: self.MangIDDV(),
