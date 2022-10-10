1386e401d00cbd0eba9575ed7a907adde9621204
NhuongDT <inftech.ts91@gmail.com>
2022-10-10 10:26:27 +0700

    - JqAuto_SearchDoiTuong: don't (locdau)

--- banhang24/Scripts/Components/NhanVien_KhachHang.js
+++ banhang24/Scripts/Components/NhanVien_KhachHang.js
@@ -308,12 +308,13 @@ var cmpChoseCustomer = {
         },
         searchDB: function () {
             var self = this;
-            var txt = locdau(self.textSearch).trim();
+            var txt = self.textSearch;
             if (commonStatisJs.CheckNull(txt)) {
                 self.customers = [];
                 this.$emit('reset-customer-parent');
                 return;
             }
+            txt = txt.trim();
             $.getJSON("/api/DanhMuc/DM_DoiTuongAPI/" + "JqAuto_SearchDoiTuong?loaiDoiTuong="
                 + self.loaiDoiTuong + "&txtSearch=" + txt + '&idChiNhanh=' + self.idChiNhanh).done(function (data) {
                     if (self.loaiDoiTuong === 1) {
