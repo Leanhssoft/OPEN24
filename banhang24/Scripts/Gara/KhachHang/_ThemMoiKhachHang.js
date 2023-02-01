d917518ee177249a79b4668b35bf06c0c1830c6e
NhuongDT <inftech.ts91@gmail.com>
2023-02-01 15:52:29 +0700

    - UpdateNhomKhachHang: remove check LoaiDoiTuong !=1

--- banhang24/Scripts/Gara/KhachHang/_ThemMoiKhachHang.js
+++ banhang24/Scripts/Gara/KhachHang/_ThemMoiKhachHang.js
@@ -912,14 +912,6 @@
                     })
                 }
             }
-            else {
-                let arrayIdDoiTuongDeleteNhom = [self.newCustomer.ID];
-                // delete all nhom of DoiTuon in DB
-                if(!isChuyenNhom)
-                    ajaxHelper(self.UrlDoiTuongAPI + 'DeleteAllNhom_ofDoiTuong?lstIDDoiTuong = ' + arrayIdDoiTuongDeleteNhom, 'POST', arrayIdDoiTuongDeleteNhom).done(function (x) {
-
-                })
-            }
         },
         NangNhomKhachHang: function (id) {
             var self = this;
